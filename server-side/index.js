c6ad56469790a98587b4d26d356a5053b67152ac
RastkoAmun <raciosh@gmail.com>
2023-03-06 11:52:54 -0800

    index.js

---
+++ server-side/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const { Pool } = require('pg')
+const PORT = 8080
+
+app.get('/', (req, res) => {
+
+})
+
+app.get('/', (req, res) => {
+  
+})
+
+app.get('/', (req, res) => {
+  
+})
+
+app.get('/', (req, res) => {
+  
+})
+
+
+app.listen(PORT, () => {
+  console.log(`App is listening on port ${PORT}`);
+})
